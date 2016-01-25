11baa6afff733839c3ce21d02c19cd2f47f4b590
Carla08 <dcfc@live.com.mx>
2016-01-24 19:25:17 -0700

    decoder and encoder

--- js/app.js
+++ js/app.js
@@ -1,23 +1,15 @@
 "use strict";
-var alpha = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
-var key = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
-var alpha_key = {alpha,key};
+var alphabet="a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".split(",");
 
-function decoder() {
-	var cipher_text = $('[name="cipherText"]').val();
-	console.log("Cipher Text:" + cipher_text);
-	var key = $('[name="key"]').val();
-	console.log("Key:" + cipher_text);
-	if(cipher_text!== null) {
-		console.log("Decyphering");
-	}
+function decode() {
+	var cipher_text = $('[name="cipherText"]').val().toLowerCase();
+	var plainTextInput =$("input[name='plainText']");
+	var key = parseInt($('[name="key"]').val(),10) * -1; //to negative
+	var plain_text = codeString(cipher_text, key);
+	plainTextInput.val(plain_text);
+	
 }
 
-
-// var alpha = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
-// var key = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
-// var alpha_key = {alpha,key};
-var alphabet="a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".split(",");
 
 function encode (){
   var plainText= $("input[name='plainText']").val().toLowerCase();
